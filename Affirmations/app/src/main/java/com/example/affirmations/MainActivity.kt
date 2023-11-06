cc3ffe8e587348d6239fdd3a3f344b7067734d63
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-06 16:43:33 +0600

    add AffirmationList and it modifier AffirmationCard in items

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -37,6 +39,18 @@ class MainActivity : ComponentActivity() {
                     AffirmationsApp()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }

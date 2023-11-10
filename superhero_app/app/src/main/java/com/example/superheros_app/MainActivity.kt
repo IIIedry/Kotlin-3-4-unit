053e965df65d2bd9b73089c415a0320ed0863605
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-11 03:28:37 +0600

    last function is completed SuperHeroApp

--- superhero_app/app/src/main/java/com/example/superheros_app/MainActivity.kt
+++ superhero_app/app/src/main/java/com/example/superheros_app/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheros_app.model.HeroesRepository
 import com.example.superheros_app.ui.theme.Superheros_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +33,22 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun SuperheroesApp() {
-
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        ) {
+            /* Important: It is not a good practice to access data source directly from the UI.
+            In later units you will learn how to use ViewModel in such scenarios that takes the
+            data source as a dependency and exposes heroes.
+             */
+            val heroes = HeroesRepository.heroes
+            HeroesList(heroes = heroes, contentPadding = it)
+        }
     }
 
     @OptIn(ExperimentalMaterial3Api::class)

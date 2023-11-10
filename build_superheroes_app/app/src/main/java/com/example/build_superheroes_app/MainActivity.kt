393f1934c85fed039fdc9f3d705dff5c783727c3
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-11 00:35:26 +0600

    now i switched main file and create superheroapp and comlete main part

--- build_superheroes_app/app/src/main/java/com/example/build_superheroes_app/MainActivity.kt
+++ build_superheroes_app/app/src/main/java/com/example/build_superheroes_app/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.build_superheroes_app.Model.HeroesRepository
 import com.example.build_superheroes_app.ui.theme.Build_superheroes_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +32,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SuperheroesApp() {
-
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroes = heroes, contentPadding = it)
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)

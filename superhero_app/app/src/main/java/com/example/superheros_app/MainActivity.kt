48355cea4204cba7cf0196eccb005c6dce62ee75
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-11 03:27:22 +0600

    fıll topappbar function with it idea

--- superhero_app/app/src/main/java/com/example/superheros_app/MainActivity.kt
+++ superhero_app/app/src/main/java/com/example/superheros_app/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superheros_app.ui.theme.Superheros_appTheme
 
@@ -33,9 +36,18 @@ class MainActivity : ComponentActivity() {
 
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun TopAppBar(){
-
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
     }
 
     @Preview(showBackground = true)

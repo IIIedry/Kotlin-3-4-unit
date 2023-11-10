77097885f9c35e20e5a76db157d4e9bb9e608071
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-11 00:28:44 +0600

    function herolist and herolistitem changed. Adding main conception

--- build_superheroes_app/app/src/main/java/com/example/build_superheroes_app/HeroesScreen.kt
+++ build_superheroes_app/app/src/main/java/com/example/build_superheroes_app/HeroesScreen.kt
@@ -2,23 +2,48 @@ package com.example.build_superheroes_app
 
 import android.content.res.Configuration
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.build_superheroes_app.Model.HeroesRepository
 import com.example.build_superheroes_app.ui.theme.Build_superheroes_appTheme
 
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun HeroesList(heroes: Any) {
+fun HeroesList(
+               heroes: List<Hero>,
+               modifier: Modifier = Modifier,
+               contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    val visibleState = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+    }
 
 }
 
 @Composable
-fun HeroListItem(hero: Hero) {
+fun HeroListItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier,
+    ) {
 
+    }
 }
 
 @Preview("Light Theme")
